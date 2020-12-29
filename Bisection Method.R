bf104c01dd4b51e49f61f85aeed5a29147fdc48b
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2020-12-29 20:59:42 +0100

    Add files via upload

---
+++ Bisection Method.R
@@ -0,0 +1,55 @@
+##################################################################################
+# Edit Following Function/Parameters
+##################################################################################
+
+rm(list = ls())
+
+#Options
+
+options(scipen = 999)
+options(digits =10)
+
+#Function
+
+f1 <- quote(x^3 - 2*x^2 - 5)
+
+#Parameters
+
+lower <- 2
+upper <- 4
+maxiteration <- 20
+
+##################################################################################
+# WARNING: Editting the following code may reuslt with crash! Use caution
+##################################################################################
+
+#Set Environment
+
+f1_a = eval(f1, list(x=lower))
+f1_b = eval(f1, list(x=upper))
+
+results <- data.frame(Iteration = numeric(maxiteration), x_a = numeric(maxiteration), x_b = numeric(maxiteration), x_m = numeric(maxiteration),
+                      fx_a = numeric(maxiteration), fx_b = numeric(maxiteration), fx_c = numeric(maxiteration))
+
+
+#Compute
+
+{
+if(f1_a*f1_b > 0){ 
+  print("There is no root in the range!")}
+else{
+  for(i in 1:maxiteration){
+    midpoint <- (lower + upper)/2
+    f1_a = eval(f1, list(x=lower))
+    f1_b = eval(f1, list(x=upper))
+    f1_c = eval(f1, list(x=midpoint))
+    results[i,] <- list(i, lower, upper, midpoint, f1_a, f1_b, f1_c)
+    if(f1_a*f1_c < 0){
+      upper = midpoint
+    }
+    else{
+      lower = midpoint
+    }
+  }
+}}
+print(results)
---
+++ Newton-Raphson Method.R
@@ -0,0 +1,45 @@
+##################################################################################
+# Edit Following Function/Parameters
+##################################################################################
+
+rm(list = ls())
+
+#Options
+options(scipen = 999)
+options(digits =10)
+
+#Function - Initial Values
+
+f1 <- quote((exp(x)/x)-(sin(x)/x^2)+2*x-10)
+Start <- 10
+
+#Another Example: quote((k/2)*y^2+(2/5)*kk*y^(5/2)-m*g*y-m*g*h)
+#Initial Values -> list(y=Start, k=40000, kk=40, m=95, g=9.81, h=0.43)
+
+#Parameters
+
+maxiteration <- 20
+
+##################################################################################
+# WARNING: Editting the following code may reuslt with crash! Use caution
+##################################################################################
+
+InitialValueList <- list(x=Start)
+
+Derivative <- D(f1, "x")
+f1_a = eval(f1, InitialValueList)[1]
+Df1_a = eval(Derivative, InitialValueList)[1]
+results <- data.frame(Iteration = numeric(maxiteration), x = numeric(maxiteration), NewX = numeric(maxiteration),
+                      fx = numeric(maxiteration), fDx = numeric(maxiteration))
+value <- Start
+for(i in 1:maxiteration){
+   InitialValueList[[1]] <- value
+   f1_a = eval(f1, InitialValueList)[1]
+   Df1_a = eval(Derivative, InitialValueList)[1]
+   NewX <- value - (f1_a/Df1_a)
+   results[i,] <- list(i, value, NewX, f1_a, Df1_a)
+   value <- NewX
+   if(i == maxiteration){print(results)}}
+   
+   
+   
